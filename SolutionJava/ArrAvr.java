83821e51743294f5b49d6c7cd57373665a9a90dc
ggsoon <88361127+ggsoon@users.noreply.github.com>
2022-05-28 08:11:01 +0900

    20220528

---
+++ SolutionJava/ArrAvr.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ArrAvr {
+    public static void main(String[] args) {
+        int[] inputarr = {75,95,100};
+        Solution3 So = new Solution3();
+        System.out.println(So.solution(inputarr));
+    }
+}
+
+class Solution3 {
+    public double solution(int[] arr) {
+        double answer = Arrays.stream(arr).sum();
+        return answer / arr.length;
+    }
+}
