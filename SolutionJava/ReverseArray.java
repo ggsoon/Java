1bf790fe8e70002b9a1b9a48dd9065ef4e98b5a3
ggsoon <88361127+ggsoon@users.noreply.github.com>
2022-06-02 01:43:48 +0900

    20220602

--- SolutionJava/ReverseArray.java
+++ SolutionJava/ReverseArray.java
@@ -1,11 +1,12 @@
 
 class Solution9{
    public int[] solution(long n) {
-       String temp = n + ""; // 문자열 + 숫자 = 문자열
+       String temp = "" + n;
        int[] answer = new int[temp.length()];
        int cnt = 0;
-       while(n > 0) {
-           answer[cnt] = (int) (n %= 10);
+
+       while (n > 0) {
+           answer[cnt] = (int) (n % 10);
            n /= 10;
            cnt++;
        }
