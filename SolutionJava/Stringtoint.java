c2838c42f3ed93d25169e9e64913ebc407d3416c
ggsoon <88361127+ggsoon@users.noreply.github.com>
2022-06-04 10:14:12 +0900

    20220603

---
+++ SolutionJava/Stringtoint.java
@@ -0,0 +1,8 @@
+public class Stringtoint {
+    public static void main(String[] args) {
+            int answer = 0;
+            String s = "-1234";
+            answer = Integer.parseInt(s);
+        System.out.println(answer);
+    }
+}
