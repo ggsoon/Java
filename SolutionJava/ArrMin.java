8720ba4a8bb738ad54955652d17c56cde5de902a
ggsoon <88361127+ggsoon@users.noreply.github.com>
2022-06-06 06:58:37 +0900

    20220606

--- SolutionJava/ArrMin.java
+++ SolutionJava/ArrMin.java
@@ -5,10 +5,14 @@ import java.util.Collections;
 public class ArrMin {
     public static void main(String[] args) {
         int[] answer = {4,3,2};
+       if (answer.length == 1) {
+           answer = new int[]{-1};
+           System.out.println(Arrays.toString(answer));
+       }
        int min = Arrays.stream(answer).min().getAsInt();
         for(int i=0; i<answer.length; i++) {
          if (answer[i] == min) {
-             Arrays.stream(answer).reduce();
+             answer = Arrays.stream(answer).filter(j -> j != min).toArray();
              System.out.println(Arrays.toString(answer));
          }
         }
