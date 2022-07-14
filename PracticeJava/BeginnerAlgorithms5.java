0f99b7217625a841b185cefff20162969fe30bf0
ggsoon <88361127+ggsoon@users.noreply.github.com>
2022-07-14 11:59:50 +0900

    20220714

--- PracticeJava/BeginnerAlgorithms5.java
+++ PracticeJava/BeginnerAlgorithms5.java
@@ -154,6 +154,85 @@ class Queue<E> {
     }
 }
 
+class CircleQueue {
+    private int max;
+    private int front;
+    private int rear;
+    private int num;
+    private int[] que;
+
+    public CircleQueue(int capacity) {
+        num = front = rear = 0;
+        max = capacity;
+        try {
+            que = new int[max];
+        } catch (OutOfMemoryError e) {
+            max = 0;
+        }
+    }
+
+    public int enque(int x) {
+        if (num >= max)
+            System.out.println("Overflow");
+        que[rear++] = x;
+        num++;
+        if (rear == max)
+            rear = 0;
+        return x;
+    }
+
+    public int deque() {
+        if (num <= 0)
+            System.out.println("Empty que");
+        int x = que[front++];
+        num --;
+        if (front == max)
+            front = max;
+        return x;
+    }
+
+    public int peek(int x) {
+        if (num <= 0)
+            System.out.println("Empty que");
+        for (int i =0; i < num; i++) {
+            int idx = (i + front) % max;
+            if (que[idx] == x)
+                return idx;
+        }
+        return -1;
+    }
+
+    public void clear() {
+        num = front = rear = 0;
+    }
+
+    public int capacity() {
+        return max;
+    }
+
+    public int size() {
+        return num;
+    }
+
+    public boolean isEmpty() {
+        return num <= 0;
+    }
+
+    public boolean isFull() {
+        return num >= max;
+    }
+
+    public void dump() {
+        if (num <= 0)
+            System.out.println("큐가 비어있다.");
+        else {
+            for (int i = 0; i < num; i++)
+                System.out.print(que[(i+front) % max] + " ");
+            System.out.println();
+        }
+    }
+
+}
 
 public class BeginnerAlgorithms5 {
     public static void main(String[] args) {
